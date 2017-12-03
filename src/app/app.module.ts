00eb0ff484f102649497bf96ab628ac704a19342
lalithkumart <jhon@gmail.com>
2017-12-03 17:10:42 +0530

    ROUTING - Implemented Routing concepts. Based on three URL/Tabs , the respective component view is rendered.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';  
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { practiceComponent } from './practice/practice.component';
 import { StudentComponent } from './student/student.component';
+import { practiceComponent } from './practice/practice.component';
 import { EmpComponent } from './employee/emp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    practiceComponent
-    StudentComponent
+    StudentComponent,
+    practiceComponent,
     EmpComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'practice', component: practiceComponent},
+      {path: 'student', component: StudentComponent},
+      {path: 'employee', component: EmpComponent}
+    ])
   ],
   bootstrap: [AppComponent]
 })
